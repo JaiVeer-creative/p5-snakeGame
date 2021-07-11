fa006f605e5e8f97fd264b5bb01bc4ec128d8ad3
JaiVeer-creative <73180396+JaiVeer-creative@users.noreply.github.com>
2021-07-11 11:29:26 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ let img
 var FruitSwitch=1
 var fruitX
 var fruitY
-var snakeLength = 50;
+var snakeLength = 5;
 var snakeDirection = 'up' // 'down', 'left', 'right'
 var snakeX = 200
 var snakeY = 200
@@ -20,43 +20,55 @@ function setup() {
   createCanvas(400, 400);
   background(120)
   strokeWeight(10)
-
   for(i=1;i<=snakeLength;i=i+1){
     xPosArray[i]=200
     yPosArray[i]=200
   }
-
+  rectMode(CENTER)
+  frameRate(5)
 }
 
 function draw() {
   background(120);
   if (FruitSwitch==1){
-  [fruitX,fruitY]=drawfruit(1)
+ //SPECIAL EVENT HAS OCCURRED 
+    [fruitX,fruitY]=drawfruit(1)
    FruitSwitch=0
-  mySound.play()}
-  
+  mySound.play()
+  incrementLength = 1
+  snakeLength+=incrementLength;
+    
+  for(i = 1;i<=incrementLength;i++){
+  xPosArray = concat(xPosArray,xPosArray[xPosArray.length-1]) ;
+  yPosArray = concat(yPosArray,yPosArray[yPosArray.length-1]) ;
+  }
+
+}  
+
 image(img,fruitX,fruitY,50,50)
   // change snakeDirection
-  if (key == 'ArrowUp'||key == 'w'){
+  if (key == 'ArrowUp'||key == 'w'||key=='W'){
     snakeDirection ='up'
   }
-  else if(key == 'ArrowDown'||key == 's'){
+  else if(key == 'ArrowDown'||key == 's'||key=='S'){
     snakeDirection = 'down'
   }
   if (key=='d'||key == 'ArrowRight'||key=='D'){
   snakeDirection = 'right'  
   }
-   if (key=='a'||key == 'ArrowLeft'){
+   if (key=='a'||key == 'ArrowLeft'||key=='A'){
   snakeDirection = 'left'  
   }
   print(snakeDirection)
   print(key)
   
   
+  
+
   // moveSnake
   // Update last point
   if (snakeDirection == 'up')  {
-    yPosArray[snakeLength] = yPosArray[snakeLength]-1
+    yPosArray[snakeLength] = yPosArray[snakeLength]-10
     xPosArray[snakeLength] = xPosArray[snakeLength] 
     if(yPosArray[snakeLength]<0){
     yPosArray[snakeLength]=height
@@ -64,7 +76,7 @@ image(img,fruitX,fruitY,50,50)
   }
   
     if (snakeDirection == 'down')  {
-    yPosArray[snakeLength] = yPosArray[snakeLength]+1
+    yPosArray[snakeLength] = yPosArray[snakeLength]+10
     xPosArray[snakeLength] = xPosArray[snakeLength]
          if(yPosArray[snakeLength]>height){
     yPosArray[snakeLength]=0
@@ -73,14 +85,14 @@ image(img,fruitX,fruitY,50,50)
    }
     if (snakeDirection == 'left')  {
     yPosArray[snakeLength] = yPosArray[snakeLength]
-    xPosArray[snakeLength] = xPosArray[snakeLength]-1
+    xPosArray[snakeLength] = xPosArray[snakeLength]-10
     if(xPosArray[snakeLength]<0){
     xPosArray[snakeLength]=width
     }
     }
    if (snakeDirection == 'right')  {
     yPosArray[snakeLength] = yPosArray[snakeLength]
-    xPosArray[snakeLength] = xPosArray[snakeLength]+1
+    xPosArray[snakeLength] = xPosArray[snakeLength]+10
         if(xPosArray[snakeLength]>width){
     xPosArray[snakeLength]=1
     }
@@ -92,11 +104,36 @@ image(img,fruitX,fruitY,50,50)
     xPosArray[i] = xPosArray[i+1]
   } 
   //drawSnake
+  
   for(i = 1; i <=snakeLength; i= i+1){
-  point(xPosArray[i],yPosArray[i])
+  strokeWeight(0)
+  fill(255)
+  circle(xPosArray[i],yPosArray[i],10)
   }
-
+  
   mouthX = xPosArray[snakeLength]
   mouthY = yPosArray[snakeLength]
+  
+  fill(0)
+  if (snakeDirection =='up' || snakeDirection =='Down'){
+  rect(mouthX,mouthY-5,5,30)}
+  else{
+  rect(mouthX-5,mouthY,30,5)}
+  
+  
+  fill(255,0,0)
+  circle(mouthX,mouthY,20)
+  
+  
+  fill(255)
   FruitSwitch = touch(mouthX,mouthY,fruitX,fruitY)
+  
+  
+  if (FruitSwitch==1){
+  fill(255,0,0)
+  for (i=0;i<xPosArray.length;i++){
+    circle(xPosArray[i],yPosArray[i],10)
+  }
+  fill(255)
+  }
 }
